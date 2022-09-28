3b25d1b89641027f5a84335f18f30f5d683e8108
Marrowsed <marcus.ah@gmail.com>
2022-09-28 08:52:24 -0300

    urls

--- back/urls.py
+++ back/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 
+from .views import *
 urlpatterns = [
-    #path('', ),
+    path('banks/', BankView.as_view(), name='bank-view'),
+    path('banks/<str:pk>/', BankDetail.as_view(), name='bank-detail'),
+    path('accounts/', AccountView.as_view(), name='account-view'),
+    path('accounts/<str:pk>', AccountDetail.as_view(), name='account-detail'),
+    path('movements/', MovementView.as_view(), name='movement-view'),
+    path('movements/<str:pk>', MovementDetail.as_view(), name='movement-detail'),
 ]
