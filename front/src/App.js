72ec9225139f793f4bf5ceed12e6fd4344f476ff
Marrowsed <marcus.ah@gmail.com>
2022-09-29 04:08:03 -0300

    react files

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container } from 'react-bootstrap'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+import React from 'react';
+
+
+import Header from './components/Header'
+import HomeScreen from './screens/HomeScreen'
+import BankScreen from './screens/BankScreen'
+import MovementScreen from './screens/MovementScreen'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Container>
+        <Routes>
+          <Route path="/" element={<HomeScreen/>} exact />
+          <Route path="/banks" element={<BankScreen/>} />
+          <Route path="/movements" element={<MovementScreen/>} />
+        </Routes>
+      </Container>
+    </Router>
   );
 }
 
