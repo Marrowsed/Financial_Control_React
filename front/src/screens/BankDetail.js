b2edd42209681bac2bc3e120675a0434af66280d
Marrowsed <marcus.ah@gmail.com>
2022-09-30 18:55:29 -0300

    bank screen

---
+++ front/src/screens/BankDetail.js
@@ -0,0 +1,38 @@
+import React, {useEffect, useState} from 'react';
+import {useParams} from 'react-router-dom';
+import Bank from '../components/Bank'
+
+const BankDetail = () => {
+    const params = useParams()
+
+    const[banks, setBanks] = useState([])
+
+    useEffect(() => {
+      fetch(`http://127.0.0.1:8000/api/banks/${params.id}`)
+      .then(response => response.json())
+      .then(data => {
+        setBanks(data)
+      })
+    }, [])
+
+    return(
+        <section>
+        <div className='row ml-4 text-center'>
+        {
+        banks.map(b => 
+        (
+        <Bank 
+          key={b.id} 
+          name={b.name} 
+          image={b.image}
+          id={b.id}
+        />
+        )
+        )
+        }
+        </div>
+      </section>
+    )
+}
+
+export default BankDetail
---
+++ front/src/screens/BankList.js
@@ -0,0 +1,35 @@
+import React, {useEffect, useState} from 'react';
+import Bank from '../components/Bank'
+
+const BankList = () => {
+    const[banks, setBanks] = useState([])
+
+    useEffect(() => {
+      fetch('http://127.0.0.1:8000/api/banks/')
+      .then(response => response.json())
+      .then(data => {
+        setBanks(data)
+      })
+    }, [])
+
+    return(
+        <section>
+        <div className='row ml-4 text-center'>
+        {
+        banks.map(b => 
+        (
+        <Bank 
+          key={b.id} 
+          name={b.name} 
+          image={b.image}
+          id={b.id}
+        />
+        )
+        )
+        }
+        </div>
+      </section>
+    )
+}
+
+export default BankList
