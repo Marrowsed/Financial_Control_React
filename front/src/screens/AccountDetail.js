08bc820c615d68ca71fc02eb701779f38d0dd1c9
Marrowsed <marcus.ah@gmail.com>
2022-09-30 18:55:43 -0300

    account screen

---
+++ front/src/screens/AccountDetail.js
@@ -0,0 +1,53 @@
+//import Account from '../components/Account'
+import { LinkContainer } from 'react-router-bootstrap'
+import {useParams} from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+
+const AccountDetail = () =>{
+    const params = useParams()
+
+    const[accounts, setAccount] = useState([])
+
+    useEffect(() => {
+      fetch(`http://127.0.0.1:8000/api/${params.id}/accounts/${params.pk}/`)
+      .then(response => response.json())
+      .then(data => {
+        setAccount(data)
+      })
+    }, []) 
+
+    const getLimit = () => {
+      return "$" + parseInt(accounts.limit - accounts.get_credit)
+    }
+
+    const getSaving = () => {
+      return "$" + accounts.get_savings
+    }
+
+    const getDebit = () => {
+      return "$" + accounts.get_debit
+    }
+
+    
+    return(
+        <section>
+        <div className='row ml-4 text-center'>
+          <div className='container'>
+            <LinkContainer to={`/${params.id}/accounts`}>
+                <button className='btn btn-info'>Back to Accounts</button>
+            </LinkContainer>
+            <h1>Limit</h1>
+            <input value={`${getLimit()}`} readOnly/>
+            <h1>Savings</h1>
+            <input value={`${getSaving()}`} readOnly/>
+            <h1>Debit </h1>
+            <input value={`${getDebit()}`} readOnly/>
+          </div>
+        </div>
+      </section>
+    )
+
+}
+
+export default AccountDetail
+
---
+++ front/src/screens/AccountList.js
@@ -0,0 +1,55 @@
+import Account from '../components/Account'
+import { LinkContainer } from 'react-router-bootstrap'
+import {useParams} from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+
+const AccountList = () =>{
+    const params = useParams()
+
+    const[accounts, setAccount] = useState([])
+
+    useEffect(() => {
+      fetch(`http://127.0.0.1:8000/api/${params.id}/accounts/`)
+      .then(response => response.json())
+      .then(data => {
+        setAccount(data)
+        console.log(data)
+      })
+    }, [])
+
+    return(
+        <section>
+            <LinkContainer to={`/banks`}>
+                <button className='btn btn-info'>Back to Banks</button>
+            </LinkContainer>
+        <div className='row ml-4 text-center'>
+        <table className='table table-hover'>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Total Limit</th>
+                    <th></th>
+                </tr>
+            </thead>
+        {
+        accounts.map(a => 
+        (
+        <Account 
+          key={a.id} 
+          name={a.name}
+          bank={a.bank}
+          limit={a.limit}
+          id={a.id}
+        />
+        )
+        )
+        }
+        </table>
+        </div>
+      </section>
+    )
+
+}
+
+export default AccountList
+
