a1b71d4f980abfa4d6810cd2a8829ced38383b87
Marrowsed <marcus.ah@gmail.com>
2022-10-02 22:51:55 -0300

    movement update

--- front/src/screens/MovementList.js
+++ front/src/screens/MovementList.js
@@ -3,91 +3,59 @@ import Movement from '../components/Movement'
 
 
 import React, {useEffect, useState} from 'react';
-import {useParams, useSearchParams, useNavigate} from 'react-router-dom';
-import { Button, Form } from 'react-bootstrap'
+import {useParams} from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap'
 
 const MovementList = () =>{
 
     const params = useParams()
+    let date, mid_date, final_date = "";
 
     const[movements, setMovement] = useState([])
-    const[searchParams, setSearchParams] = useSearchParams()
-
-    const searchHandler = (event) => {
-      let search;
-      if (event.target.value) {
-        search = {
-          created_at: event.target.value
-        }
-      } else {
-        search = undefined;
-      }
-  
-      setSearchParams(search, { replace: true });
-    }
-
-    const navigate = useNavigate();
-
-    const submitHandler = (e) => {
-        e.preventDefault()
-        if (searchParams) {
-            navigate(`?${searchParams}`)
-        }
-         else {
-            alert("ERROR")
-        }
-      }
-
+    
     useEffect(() => {
-        fetch(`http://127.0.0.1:8000/api/${params.id}/movements?${searchParams}`)
+        fetch(`http://127.0.0.1:8000/api/${params.id}/movements`)
         .then(response => response.json())
         .then(data => {
           setMovement(data)
       })
-    }, [params.id, searchParams])
+    }, [params.id])
+    
+    date = movements.created_at
+    mid_date = date.split('-')
+    final_date = mid_date[0] + "/" + mid_date[1]
 
     return(
         <section>
         <div className='row ml-4 text-center'>
-        <Form onSubmit={submitHandler} inline>
-            <input
-              type='date'
-              value={searchParams.created_at}
-              onChange={searchHandler}
-              placeholder='Search...'
-              style={{ width: '300px', padding: '8px 15px', fontSize: '16px' }}
-        />
-            <Button
-                type='submit'
-                variant='outline-success'
-                className='p-2'
-            >Search</Button>
-        </Form>
         <table className='table table-hover'>
             <thead>
                 <tr>
-                    <th>Type</th>
-                    <th>Months</th>
-                    <th>Description</th>
-                    <th>Value</th>
-                    <th>Created At</th>
+                    <th>Month/Year</th>
+                    <th>Total Debit</th>
+                    <th>Total Credit</th>
+                    <th>Total Savings</th>
+                    <th></th>
                 </tr>
             </thead>
         {
-        movements.map(m => 
-        (
-        <Movement 
-          key={m.id} 
-          account={m.account}
-          p_choice={m.p_choice}
-          p_months={m.p_months}
-          description={m.description}
-          value={m.value}
-          created_at={m.created_at}
-        />
+        movements.map( m =>
+            <tbody>
+              <tr>
+                <td>{final_date}</td>
+                <td>Total Debit</td>
+                <td>Total Credit</td>
+                <td>Total Savings</td>
+                <td>
+                <LinkContainer to={`${final_date}`}>
+                  <button className='btn btn-outline-info mx-auto'>Detail</button>
+                </LinkContainer>
+                </td>
+              </tr>
+            </tbody>
         )
-        )
-        }
+        }    
+        
         </table>
         </div>
       </section>
